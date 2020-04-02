3ee39b65effcc4c7ce48cba01cb78a80f33c9a8f
Шанин Артём Юрьевич <joymefistoggc@gmail.com>
2020-04-02 20:54:54 +0300

    simples vdom

---
+++ src/js/index.babel.js
@@ -0,0 +1,70 @@
+/** @jsx h */
+function h(type, props, ...children) {
+  return {
+    type,
+    props,
+    children
+  };
+}
+/**
+ * Отрисовка элемента и текстового узла
+ * @param {*} node 
+ */
+
+
+function createElement(node) {
+  if (typeof node === 'string') {
+    return document.createTextNode(node);
+  }
+
+  const $el = document.createElement(node.type);
+  node.children.map(createElement).forEach($el.appendChild.bind($el));
+  return $el;
+}
+/**
+ * Обновление узла
+ * @param {*} $parent 
+ * @param {*} newNode 
+ * @param {*} oldNode 
+ * @param {*} index 
+ */
+
+
+function updateElement($parent, newNode, oldNode, index = 0) {
+  if (!oldNode) {
+    $parent.appendChild(createElement(newNode));
+  } else if (!newNode) {
+    $parent.removeChild($parent.childNodes[index]);
+  } else if (changed(newNode, oldNode)) {
+    $parent.replaceChild(createElement(newNode), $parent.childNodes[index]);
+  } else if (newNode.type) {
+    const newLength = newNode.children.length;
+    const oldLength = oldNode.children.length;
+
+    for (let i = 0; i < newLength || i < oldLength; i++) {
+      updateElement($parent.childNodes[index], newNode.children[i], oldNode.children[i], i);
+    }
+  }
+}
+/**
+ * Сравнение двух узлов, будь то настоящий элемент или текстовый узел
+ * @param {*} node1 
+ * @param {*} node2 
+ */
+
+
+function changed(node1, node2) {
+  return typeof node1 !== typeof node2 || typeof node1 === 'string' && node1 !== node2 || node1.type !== node2.type;
+} // ---------------------------------------------------------------------
+
+
+const a = h("ul", {
+  class: "list"
+}, h("li", null, "item 1"), h("li", null, "item 2"));
+const b = h("ul", null, h("li", null, "item 1"), h("li", null, "hello!"));
+const $root = document.getElementById('root');
+const $reload = document.getElementById('reload');
+updateElement($root, a);
+$reload.addEventListener('click', () => {
+  updateElement($root, b, a);
+});
